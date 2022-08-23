97d564974ca4fd8ccf8bbd096367a2929bf851f1
Techwrdd <107527532+Techwrdd@users.noreply.github.com>
2022-08-23 13:13:14 +0530

    Update __main__.py

--- LogiMusic/__main__.py
+++ LogiMusic/__main__.py
@@ -73,4 +73,4 @@ async def init():
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("LogiMusic").info("Stopping Music Bot, Bhakk Bhosdike (Gaand Maraa Tu)")
+    LOGGER("LogiMusic").info("Stopping Music Bot")
